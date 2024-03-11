3a9b57c17e6763c48ff1b1f54ebab72407e04ebd
DmitryLiashenko <155096228+DmitryLiashenko@users.noreply.github.com>
2024-03-11 16:57:07 +0200

    Well

--- hw7.py
+++ hw7.py
@@ -1,8 +1,6 @@
 import pickle
-import datetime as dt
 from dataclasses import dataclass
-from datetime import timedelta as td
-from datetime import datetime as dtdt
+from gup import get_upcoming_birthdays
 
 
 def input_error(func):
@@ -21,29 +19,29 @@ def input_error(func):
     return inner
 
 
-@input_error
-def get_upcoming_birthdays(book):
-    """Get upcoming birthdays data."""
-    congratulation_data = []
-    today = dtdt.today().date()
-    future_day = today + td(days=7)
-    for record in book.values():
-        if record.birthday:
-            birthday = record.birthday.value
-            birthday_date = dt.date(today.year, birthday.month, birthday.day)
-            if (today < birthday_date) and (birthday_date <= future_day):
-                if birthday_date.weekday() == 5:
-                    birthday_date = birthday_date + td(days=2)
-                    congratulation_data.append({'name': record.name.value,
-                                                'congratulation_date': birthday_date.strftime("%Y-%m-%d")})
-                elif birthday_date.weekday() == 6:
-                    birthday_date = birthday_date + td(days=1)
-                    congratulation_data.append({'name': record.name.value,
-                                                'congratulation_date': birthday_date.strftime("%Y-%m-%d")})
-                else:
-                    congratulation_data.append({'name': record.name.value,
-                                                'congratulation_date': birthday_date.strftime("%Y-%m-%d")})
-    return congratulation_data
+# @input_error
+# def get_upcoming_birthdays(book):
+#     """Get upcoming birthdays data."""
+#     congratulation_data = []
+#     today = dtdt.today().date()
+#     future_day = today + td(days=7)
+#     for record in book.values():
+#         if record.birthday:
+#             birthday = record.birthday.value
+#             birthday_date = dt.date(today.year, birthday.month, birthday.day)
+#             if (today < birthday_date) and (birthday_date <= future_day):
+#                 if birthday_date.weekday() == 5:
+#                     birthday_date = birthday_date + td(days=2)
+#                     congratulation_data.append({'name': record.name.value,
+#                                                 'congratulation_date': birthday_date.strftime("%Y-%m-%d")})
+#                 elif birthday_date.weekday() == 6:
+#                     birthday_date = birthday_date + td(days=1)
+#                     congratulation_data.append({'name': record.name.value,
+#                                                 'congratulation_date': birthday_date.strftime("%Y-%m-%d")})
+#                 else:
+#                     congratulation_data.append({'name': record.name.value,
+#                                                 'congratulation_date': birthday_date.strftime("%Y-%m-%d")})
+#     return congratulation_data
 
 
 @dataclass
